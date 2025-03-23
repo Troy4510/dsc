45747f2ac4582c342d5a6fc59ab4be5e7d93745d
troy4510 <iceman.cybercore@gmail.com>
2025-03-23 16:54:58 +0200

    requirements

--- webstart.py
+++ webstart.py
@@ -10,5 +10,15 @@ main_folder = './main/'
 def index():
    return render_template('index.html')
 
+@app.route('/user/<int:user_id>/')
+def user_profile(user_id):
+    return "Profile page of user #{}".format(user_id)
+
+@app.route('/login/')
+def register_user():
+   text = 'ВХОД В DSChat:'
+   return render_template('login.html', message = text, addr = request.remote_addr, cli = request.user_agent)
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5050, debug=True)
